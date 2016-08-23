8658898f8e50af72d51eac520063d448220a9aad
ohhhi <wafflesluv6678@gmail.com>
2016-08-23 12:57:16 -0700

    Update __init__.py

--- kik/__init__.py
+++ kik/__init__.py
@@ -3,6 +3,7 @@ from kik.api import KikApi
 from kik.code import Code
 from kik.configuration import Configuration
 from kik.error import KikError
-from kik.user import User
+from kik.user import kawaii.bot
+
 
 __all__ = ['messages', 'KikApi', 'Code', 'Configuration', 'KikError', 'User']
